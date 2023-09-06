e91c8612c1ecf3536a33c320b7f2f81e20f90d15
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-09-06 20:57:58 +0300

    add environment

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Andrey:XS2w4F6c1uHcNTnR@cluster0.sesomu1.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     });
   })
