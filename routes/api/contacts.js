4a8f1237ee41bfb34b89983a9f31092461a97660
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-09-07 22:56:49 +0300

    add models

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+// const { validateBody } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+// const schemas = require("../../schemas/contacts");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+// router.get("/:contactId", ctrl.getById);
 
-router.post("/",validateBody(schemas.addSchema), ctrl.add);
+// router.post("/",validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+// router.delete("/:contactId", ctrl.deleteById);
 
-router.put("/:contactId",validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:contactId",validateBody(schemas.addSchema), ctrl.updateById);
 
 module.exports = router;
