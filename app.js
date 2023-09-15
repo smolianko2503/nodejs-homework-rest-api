494edba1f2fc254d66bdb5b2d9c72fafa9bd9146
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-09-15 22:56:52 +0300

    add user model

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const authRouter = require("./routes/api/auth");
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -14,6 +16,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.use("/api/contacts", contactsRouter);
 
---
+++ models/user.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require("mongoose");
+
+const Joi = require("joi");
+
+const { regexpEmail } = require("../schemas/regexps");
+
+const { handleMongooseError } = require("../helpers");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: regexpEmail,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(regexpEmail).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(regexpEmail).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas
+}
+
