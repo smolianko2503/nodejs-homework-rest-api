b5750da8b234a0cb5aceedb24a14ca1fb47212b7
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-09-16 12:07:07 +0300

    add authenticate.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,53 @@
 const { User } = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-const register = async (req, res) => {
-    const newUser = await User.create(req.body);
+const { SECRET_KEY } = process.env;
 
-    res.status(201).json({
-        email: newUser.email,
-        name: newUser.name,
-    })
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+
+  res.json({
+    token,
+  });
 };
 
 module.exports = {
-    register: ctrlWrapper(register),
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,4 +12,6 @@ const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login)
+
 module.exports = router;
